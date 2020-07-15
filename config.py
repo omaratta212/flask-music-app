c53c169a47e2234258c99800cce1d02ce1f8a7d4
Atta <omaratta212@gmail.com>
2020-07-15 17:03:45 +0200

    Connect to the db

--- config.py
+++ config.py
@@ -9,5 +9,4 @@ DEBUG = True
 # Connect to the database
 
 
-# TODO IMPLEMENT DATABASE URL
-SQLALCHEMY_DATABASE_URI = 'postgresql://atta:passwordforatta@127.0.0.1:5432/todoapp'
+SQLALCHEMY_DATABASE_URI = 'postgresql://atta:passwordforatta@127.0.0.1:5432/fyyur'
